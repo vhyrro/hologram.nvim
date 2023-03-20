669860f9f1b101e11cd624ba872cabd99f7e8298
Vhyrro <vhyrro@gmail.com>
2023-03-20 16:29:37 +0100

    feat: display images on buffer enter

--- lua/hologram/init.lua
+++ lua/hologram/init.lua
@@ -71,6 +71,59 @@ function hologram.setup(opts)
             end
         end,
     })
+
+    if not opts.auto_display then
+        return
+    end
+
+    local managed_buffers = {}
+
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+        group = group,
+        callback = function(event)
+            local buffer = event.buf
+
+            if managed_buffers[buffer] then
+                return
+            end
+
+            local query = vim.treesitter.get_query(vim.api.nvim_buf_get_option(buffer, "filetype") or "", "images")
+
+            if not query then
+                return
+            end
+
+            local top_level_node = vim.treesitter.get_parser(buffer):parse()[1]
+
+            if not top_level_node then
+                return
+            end
+
+            for capture_id, node in query:iter_captures(top_level_node:root(), buffer) do
+                local capture = query.captures[capture_id]
+
+                if capture == "path" then
+                    local path = vim.treesitter.get_node_text(node, buffer)
+
+                    if not path then
+                        return
+                    end
+
+                    -- For now, hardcode the behaviour such that the image gets placed one line
+                    -- below the path. This should be customizable in the future.
+                    local start, col = node:range()
+
+                    local image = Image:new(path)
+
+                    if image then
+                        image:display(start + 1, col, buffer, {})
+                    end
+                end
+            end
+
+            managed_buffers[buffer] = true
+        end,
+    })
 end
 
 return hologram
---
+++ queries/norg/images.scm
@@ -0,0 +1,4 @@
+(infirm_tag
+  (tag_name) @name
+  (tag_parameters) @path
+  (#eq? name "image"))
